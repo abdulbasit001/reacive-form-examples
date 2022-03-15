e26ecd981e7f8f385ef79c3d7496e0b6ffd7c59a
abdulbasit001 <abdul.basit@shottechnologies.co>
2022-03-15 21:23:30 +0500

    update read me md files

---
+++ src/app/Components/user/user.component.ts
@@ -0,0 +1,14 @@
+
+export class UserComponent{
+
+  constructor(
+    public name:string,
+    public email:string,
+    public phone:number,
+    public topic:string,
+    public timePreference:string,
+    public subscribed:boolean,
+  ) { }
+
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { UserComponent } from './Components/user/user.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'form1';
+  public courses = ['React', 'Angular', 'Vue'];
+  constructor() {}
+  public submitForm(event: any) {
+    // console.log(userForm.value | json);
+  }
+  useModel = new UserComponent('Rob', 'rob@test.com', 5545545, 'react', 'morning', true);
 }
